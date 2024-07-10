763c88291e2c804180f7693a0f13e3fb0fd8eb75
lizan19920214 <894295412@qq.com>
2024-07-10 17:43:39 +0800

    添加一些范围for循环的使用细节

--- c++11/for/test.cpp
+++ c++11/for/test.cpp
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <iostream>
+#include <set>
 
 void test1()
 {
@@ -14,7 +15,6 @@ void test1()
 }
 
 // 变得非常的简单：
-
 void test2()
 {
     std::vector<int> arr(5, 100);
@@ -24,9 +24,29 @@ void test2()
     }
 }
 
+//需要注意的细节
+void test3()
+{
+    // 1、使用for范围循环同样要注意容器本身的约束，比如set容器为只读，如下就会报错
+    std::set<int> set = {1, 2, 3, 4, 5};
+    for(auto& n : set) 
+    {    
+        // n++; error
+    }
+
+    // 2、循环的vector插入数据同样出现迭代器失效
+    std::vector<int> arr(5, 100);
+    for(auto& i : arr) 
+    {    
+        i = 0;
+        // arr.push_back(3); 迭代器失效
+    }
+}
+
 int main()
 {
     test1();
     test2();
+    test3();
     return 0;
 }
